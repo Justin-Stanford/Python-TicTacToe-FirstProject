5a74faa01763319dc24883575fae8a3d278c006e
Justin-Stanford <justinstanford16@gmail.com>
2023-07-13 23:49:11 -0700

    testing

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ while running:
     # RENDER YOUR GAME HERE
     for x in range(1,3): #each cell is going to be sizex / 3, sizey / 3
         for y in range(1,3):
-            pygame.draw.line(screen,"white",((SIZEX / 3 * x), (SIZEY / 3 * y)))
+            
 
     # flip() the display to put your work on screen
     pygame.display.flip()
@@ -32,3 +32,4 @@ while running:
     clock.tick(60)  # limits FPS to 60
 
 pygame.quit()
+l
